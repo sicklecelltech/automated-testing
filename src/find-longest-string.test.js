c814ba17903767dcd50bc652210ba283ca8e08fd
Tiffany Wilmot-Simpson <twilmotsimpson@gmail.com>
2022-02-23 17:14:37 -0500

    Added 2 more test cases

--- src/find-longest-string.test.js
+++ src/find-longest-string.test.js
@@ -8,4 +8,6 @@ test("findLongestString finds the longest string in an array", () => {
 
 test("findLongestString returns the earlier string in cases of joint longest strings", () => {
   expect(findLongestString(["brave", "dance"])).toBe("brave");
+  expect(findLongestString(["xylophone","goblet", "alphabets", "booker"])).toBe("xylophone");
+  expect(findLongestString(["a","b","c","d","e"])).toBe("a");
 });
